580315ad4e02453f75428a040d6a15d59ed87f0d
ZHENG YUNKAI <769440615@>
2026-04-14 08:32:16 +0800

    Initial Commit

---
+++ findTests/findTests.swift
@@ -0,0 +1,19 @@
+//
+//  findTests.swift
+//  findTests
+//
+//  Created by 郑云凯 on 2026/4/14.
+//
+
+import Testing
+@testable import find
+
+struct findTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
